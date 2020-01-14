bcdd8142399e4196cfa118ba587f31b30dd24086
haishisai <haishisai@qq.com>
2020-01-14 19:49:02 +0800

    swiper

---
+++ vue-cli3-test/vue-swiper/src/components/swiper.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide>
+        11111
+      </swiper-slide>
+      <swiper-slide>
+        11111222
+      </swiper-slide>
+      <swiper-slide>
+        11111333
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  //采坑 name不能用 swiper
+  name: "hswiper",
+  data () {
+    return {
+      swiperOption: {
+        
+      }
+    
+    };
+  }
+}
+</script>
+<style lang="stylus" scoped></style>
---
+++ vue-cli3-test/vue-swiper/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    swiper
+    <swiper></swiper>
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+import swiper from '../components/swiper.vue';
+export default {
+  name: 'home',
+  components: {
+    swiper
+  }
+}
+</script>
