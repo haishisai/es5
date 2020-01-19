9070c0631a3aae25ddaeaa2278d974ebcbdc3959
haishisai <haishisai@qq.com>
2020-01-19 19:00:06 +0800

    save

---
+++ vue-cli3-test/swiper4/src/components/swiper1.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <swiper :options="swiperOption" ref="mySwiper" @someSwiperEvent="callback">
+      <!-- slides -->
+      <swiper-slide>I'm Slide 1</swiper-slide>
+      <swiper-slide>I'm Slide 2</swiper-slide>
+      <swiper-slide>I'm Slide 3</swiper-slide>
+      <swiper-slide>I'm Slide 4</swiper-slide>
+      <swiper-slide>I'm Slide 5</swiper-slide>
+      <swiper-slide>I'm Slide 6</swiper-slide>
+      <swiper-slide>I'm Slide 7</swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination" slot="pagination"></div>
+      
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: "swiper11",
+  data () {
+      return {
+        swiperOption: {
+            pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+          }
+        }
+      }
+    }
+}
+</script>
+<style scoped>
+</style>
