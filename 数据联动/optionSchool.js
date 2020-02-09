54349c94921cb208d67ff26491c17c93727cef0c
haishisai <haishisai@qq.com>
2020-02-09 11:28:09 +0800

    save

--- 数据联动/optionSchool.js
+++ 数据联动/optionSchool.js
@@ -1,74 +1,74 @@
-var selectSchool = (function (e) {   
-    var provinceDom = document.getElementsByClassName('province')[0];
-    var cityDom = document.getElementsByClassName('city')[0];
-    var schoolDom = document.getElementsByClassName('school')[0];
+var selectSchool = (function (e) {
+    var provinceNode = document.getElementsByClassName('province')[0];
+    var cityNode = document.getElementsByClassName('city')[0];
+    var schoolNode = document.getElementsByClassName('school')[0];
     var provinceStr = '';
     var cityStr = '';
-    var schoolStr = '';   
+    var schoolStr = '';
     // 省
-    for (var i = 0; i < province.length; i++) {        
+    for (var i = 0; i < province.length; i++) {
         provinceStr += `<option value="${province[i][0]}">${province[i][1]}</option>`
     }
-    provinceDom.innerHTML = provinceStr;
+    provinceNode.innerHTML = provinceStr;
     // 城
-    cityData = city[provinceDom.value]   // 关键 联动在这里 provinceDom.value 获取省份select的value值
-    for (var i = 0; i < cityData.length; i++) {        
+    cityData = city[provinceNode.value]   // 关键 联动在这里 provinceNode.value 获取省份select的value值
+    for (var i = 0; i < cityData.length; i++) {
         cityStr += `<option value="${cityData[i][0]}">${cityData[i][1]}</option>`
     }
     cityStr += `<option value="other">其他</option>`
-    cityDom.innerHTML = cityStr
+    cityNode.innerHTML = cityStr
     // 学校
-    schoolData = allschool[cityDom.value]
-    for (var i = 0; i < schoolData.length; i++) {        
+    schoolData = allschool[cityNode.value]
+    for (var i = 0; i < schoolData.length; i++) {
         schoolStr += `<option value="">${schoolData[i][2]}</option>`
     }
-    schoolDom.innerHTML = schoolStr
+    schoolNode.innerHTML = schoolStr
     // 省份的联动
-    provinceDom.onchange = function () {     
+    provinceNode.onchange = function () {
         cityStr = '';
         schoolStr = '';
         // 城
-        cityData = city[provinceDom.value]   // 关键 联动在这里 provinceDom.value 获取省份select的value值
-        for (var i = 0; i < cityData.length; i++) {        
+        cityData = city[provinceNode.value]   // 关键 联动在这里 provinceNode.value 获取省份select的value值
+        for (var i = 0; i < cityData.length; i++) {
             cityStr += `<option value="${cityData[i][0]}">${cityData[i][1]}</option>`
         }
         cityStr += `<option value="other">其 他</option>`
-        cityDom.innerHTML = cityStr
+        cityNode.innerHTML = cityStr
         // 学校
-        schoolData = allschool[cityDom.value]
-        for (var i = 0; i < schoolData.length; i++) {        
+        schoolData = allschool[cityNode.value]
+        for (var i = 0; i < schoolData.length; i++) {
             schoolStr += `<option value="">${schoolData[i][2]}</option>`
         }
         schoolStr += `<option value="other">其 他</option>`
-        schoolDom.innerHTML = schoolStr        
+        schoolNode.innerHTML = schoolStr
     }
     // 城市的联动
-    cityDom.onchange = function () {   
+    cityNode.onchange = function () {
         schoolStr = ''
         // 学校
-        schoolData = allschool[cityDom.value]
-        
-        if (cityDom.value == 'other') {
+        schoolData = allschool[cityNode.value]
+
+        if (cityNode.value == 'other') {
             schoolStr = `<option value="">其 他</option>`
         }
-        if(schoolData){
-            for (var i = 0; i < schoolData.length; i++) {        
+        if (schoolData) {
+            for (var i = 0; i < schoolData.length; i++) {
                 schoolStr += `<option value="">${schoolData[i][2]}</option>`
             }
             schoolStr += `<option value="other">其 他</option>`
-        }        
-        schoolDom.innerHTML = schoolStr        
-    }   
+        }
+        schoolNode.innerHTML = schoolStr
+    }
 }());
 
 
 
 // 总结 传参入口位置被固定了 函数间的传值不方便   不如对象的写法
 
-    // optionDom = document.createElement('div');
-    // optionDom.classList.add('test1')
-    // optionDom.innerHTML = 'ccc'
-    // provinceDom.appendChild(optionDom)
+    // optionNode = document.createElement('div');
+    // optionNode.classList.add('test1')
+    // optionNode.innerHTML = 'ccc'
+    // provinceNode.appendChild(optionNode)
     // appendChild 有点不灵活 需要按部就班
 
 
